a6c4b6089e22f3173d834187ed47c0288a3c9839
Deem <Deem@DEEM>
2022-08-16 11:45:18 +0300

    авторизация пользователя с помощью массивов

--- TrainConsole22/Program.cs
+++ TrainConsole22/Program.cs
@@ -6,103 +6,174 @@ namespace TrainConsole22
     {
         public static void Main()
         {
-
+            bool userAuthorized;  // = AutorizeUser(); 
             {
+                userAuthorized = false;
 
+                string[] listLogin = { "Админ", "Иванова", "Петрова", "Сергеева", "Васильева", "Семёнова" };
+                string[] listPassword = { "000", "111", "222", "333", "444", "555" }; 
+
+                int autorizationAttempthCount = 0;
+                const int MAX_NUMBER_OF_AUTHORIZATION_ATTEMPTS = 3;
+                bool presenceOfAuthorization = autorizationAttempthCount < MAX_NUMBER_OF_AUTHORIZATION_ATTEMPTS;
+                
+                while (presenceOfAuthorization)
+                {
+                    string login, password; // = InputLoginAndPassword();
+                    {
+                        Console.Write("Введите логин: ");
+                        login = Console.ReadLine();
+                        Console.Write("Введите пароль: ");
+                        password = Console.ReadLine();
+                    }
+
+                    // authorized = TryAuthorizeUser(Login, Password);
+                    {
+                        int index = 0;
+
+                        while (index < listLogin.Length && index < listPassword.Length)
+                        {
+                            bool isCorrectLogin, isCorrectPassword; // = MatchLoginAndPassword
+                            {
+                                string loginByCurrentIndex = listLogin[index];
+                                isCorrectLogin = login == loginByCurrentIndex;
+                                string passwordByCurrentIndex = listPassword[index];
+                                isCorrectPassword = password == passwordByCurrentIndex;
+                            }
+
+                            if (isCorrectLogin && isCorrectPassword)
+                            {
+                                userAuthorized = true;
+                                break;
+                            }
+                            else
+                            {
+                                index++;
+                            }
+                        }
+
+
+                    }
+
+                    if (userAuthorized)
+                    {
+                        Console.WriteLine("Вы успешно авторизованы.");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Логин и пароль введены не правильно.");
+                        presenceOfAuthorization = ++autorizationAttempthCount < MAX_NUMBER_OF_AUTHORIZATION_ATTEMPTS;
+
+                        if (presenceOfAuthorization)
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Количество попыток авторизации исчерпано. Обратитесь к администратору");
+                            break;
+                        }
+                    }
+                }
+
+                Console.ReadKey();
             }
 
-            while (true)
+            // Run (userAuthorized)
             {
-                Console.WriteLine("Нажмите Enter, для начала обслуживания нового клиента");
-                Console.ReadKey();
-
-                string countryCode;
-                decimal tileQuentity, tilePrice;
-                // countryCode, tileQuntity, tilePrice = RunInputUserInterface()
+                while (userAuthorized)
                 {
-                    const string COUNTRY_CODES =
-                        "=============================================================\n" +
-                        "Азербайджан  (994)   | Киргизия   (996)   | Таджикистан (992)\n" +
-                        "Армения      (374)   | Латвия     (371)   | Туркмения   (993)\n" +
-                        "Беларусь     (375)   | Литва      (470)   | Узбекистан  (998)\n" +
-                        "Грузия       (995)   | Молдова    (373)   | Украина     (380)\n" +
-                        "Казахстан    (007K)  | Россия     (007)   | Эстония     (372)\n" +
-                        "----------------------------------------------------------";
+                    Console.WriteLine("Нажмите Enter, для начала обслуживания нового клиента");
+                    Console.ReadKey();
 
-                    Console.WriteLine(COUNTRY_CODES);
-
-                    // countryCode = ProcessCountryCode (countryCode)
+                    string countryCode;
+                    decimal tileQuentity, tilePrice;
+                    // countryCode, tileQuntity, tilePrice = RunInputUserInterface()
                     {
-                        bool countryCodeIsCorrect;
-                        do
+                        const string COUNTRY_CODES =
+                            "=============================================================\n" +
+                            "Азербайджан  (994)   | Киргизия   (996)   | Таджикистан (992)\n" +
+                            "Армения      (374)   | Латвия     (371)   | Туркмения   (993)\n" +
+                            "Беларусь     (375)   | Литва      (470)   | Узбекистан  (998)\n" +
+                            "Грузия       (995)   | Молдова    (373)   | Украина     (380)\n" +
+                            "Казахстан    (007K)  | Россия     (007)   | Эстония     (372)\n" +
+                            "----------------------------------------------------------";
+
+                        Console.WriteLine(COUNTRY_CODES);
+
+                        // countryCode = ProcessCountryCode (countryCode)
                         {
-                            Console.Write("Телефонный код страны: ");
-                            countryCode = Console.ReadLine();
-
-                            // countryCode = DetectKazakhstanCode (countryCode)
+                            bool countryCodeIsCorrect;
+                            do
                             {
-                                const string KAZAKHSTAN_CODE = "007";
-                                const string /*UPPERCASE_ENGLISH_LETTER_K = "K",*/ LOWERCASE_ENGLISH_LETTER_K = "k",
-                                             UPPERCASE_RUSSIAN_LETTER_K = "К", LOWERCASE_RUSSIAN_LETTER_K = "к";
+                                Console.Write("Телефонный код страны: ");
+                                countryCode = Console.ReadLine();
 
-                                bool countryCodeIsKazakhstanCodeAndAnyLetterK =
-                                             //countryCode == (KAZAKHSTAN_CODE + UPPERCASE_ENGLISH_LETTER_K) ||
-                                             countryCode == (KAZAKHSTAN_CODE + LOWERCASE_ENGLISH_LETTER_K) ||
-                                             countryCode == (KAZAKHSTAN_CODE + UPPERCASE_RUSSIAN_LETTER_K) ||
-                                             countryCode == (KAZAKHSTAN_CODE + LOWERCASE_RUSSIAN_LETTER_K);
+                                // countryCode = DetectKazakhstanCode (countryCode)
+                                {
+                                    const string KAZAKHSTAN_CODE = "007";
+                                    const string /*UPPERCASE_ENGLISH_LETTER_K = "K",*/ LOWERCASE_ENGLISH_LETTER_K = "k",
+                                                 UPPERCASE_RUSSIAN_LETTER_K = "К", LOWERCASE_RUSSIAN_LETTER_K = "к";
 
-                                if (countryCodeIsKazakhstanCodeAndAnyLetterK)
+                                    bool countryCodeIsKazakhstanCodeAndAnyLetterK =
+                                                 //countryCode == (KAZAKHSTAN_CODE + UPPERCASE_ENGLISH_LETTER_K) ||
+                                                 countryCode == (KAZAKHSTAN_CODE + LOWERCASE_ENGLISH_LETTER_K) ||
+                                                 countryCode == (KAZAKHSTAN_CODE + UPPERCASE_RUSSIAN_LETTER_K) ||
+                                                 countryCode == (KAZAKHSTAN_CODE + LOWERCASE_RUSSIAN_LETTER_K);
+
+                                    if (countryCodeIsKazakhstanCodeAndAnyLetterK)
+                                    {
+                                        const string UPPERCASE_ENGLISH_LETTER_K = "K";
+                                        countryCode = KAZAKHSTAN_CODE + UPPERCASE_ENGLISH_LETTER_K;
+                                    }
+                                }
+
+                                // countryCodeIsCorrect = CheckCountryCode (countryCode)
                                 {
                                     const string UPPERCASE_ENGLISH_LETTER_K = "K";
-                                    countryCode = KAZAKHSTAN_CODE + UPPERCASE_ENGLISH_LETTER_K;
-                                }
-                            }
+                                    const string AZERBAJAN_CODE = "994", ARMENIA_CODE = "374", BELARUS_CODE = "375",
+                                                 GEORGIA_CODE = "995", KAZAKHSTAN_CODE = "007" + UPPERCASE_ENGLISH_LETTER_K,
+                                                 KYRGYSTAN_CODE = "996", LATVIA_CODE = "371", LITHUANIA_CODE = "370",
+                                                 MOLDOVA_CODE = "373", RUSSIA_CODE = "007", TAJIKISTAN_CODE = "992", TURMENISTAN_CODE = "993",
+                                                 UZBEKISTAN_CODE = "998", UKRAINE_CODE = "380", ESTONIA_CODE = "372";
 
-                            // countryCodeIsCorrect = CheckCountryCode (countryCode)
-                            {
-                                const string UPPERCASE_ENGLISH_LETTER_K = "K";
-                                const string AZERBAJAN_CODE = "994", ARMENIA_CODE = "374", BELARUS_CODE = "375",
-                                             GEORGIA_CODE = "995", KAZAKHSTAN_CODE = "007" + UPPERCASE_ENGLISH_LETTER_K,
-                                             KYRGYSTAN_CODE = "996", LATVIA_CODE = "371", LITHUANIA_CODE = "370",
-                                             MOLDOVA_CODE = "373", RUSSIA_CODE = "007", TAJIKISTAN_CODE = "992", TURMENISTAN_CODE = "993",
-                                             UZBEKISTAN_CODE = "998", UKRAINE_CODE = "380", ESTONIA_CODE = "372";
+                                    switch (countryCode)
+                                    {
+                                        case AZERBAJAN_CODE:
+                                        case ARMENIA_CODE:
+                                        case BELARUS_CODE:
+                                        case UKRAINE_CODE:
+                                        case RUSSIA_CODE:
+                                        case GEORGIA_CODE:
+                                        case KAZAKHSTAN_CODE:
+                                        case KYRGYSTAN_CODE:
+                                        case LATVIA_CODE:
+                                        case LITHUANIA_CODE:
+                                        case ESTONIA_CODE:
+                                        case MOLDOVA_CODE:
+                                        case TAJIKISTAN_CODE:
+                                        case TURMENISTAN_CODE:
+                                        case UZBEKISTAN_CODE:
+                                            {
+                                                countryCodeIsCorrect = true;
+                                                break;
+                                            }
+                                        default:
+                                            {
+                                                countryCodeIsCorrect = false;
+                                                Console.ForegroundColor = ConsoleColor.Red;
+                                                Console.WriteLine($"Вы ввели несуществующий код: {countryCode}");
+                                                Console.ResetColor();
+                                                break;
+                                            }
+                                    }
 
-                                switch (countryCode)
-                                {
-                                    case AZERBAJAN_CODE:
-                                    case ARMENIA_CODE:
-                                    case BELARUS_CODE:
-                                    case UKRAINE_CODE:
-                                    case RUSSIA_CODE:
-                                    case GEORGIA_CODE:
-                                    case KAZAKHSTAN_CODE:
-                                    case KYRGYSTAN_CODE:
-                                    case LATVIA_CODE:
-                                    case LITHUANIA_CODE:
-                                    case ESTONIA_CODE:
-                                    case MOLDOVA_CODE:
-                                    case TAJIKISTAN_CODE:
-                                    case TURMENISTAN_CODE:
-                                    case UZBEKISTAN_CODE:
-                                        {
-                                            countryCodeIsCorrect = true;
-                                            break;
-                                        }
-                                    default:
-                                        {
-                                            countryCodeIsCorrect = false;
-                                            Console.ForegroundColor = ConsoleColor.Red;
-                                            Console.WriteLine($"Вы ввели несуществующий код: {countryCode}");
-                                            Console.ResetColor();
-                                            break;
-                                        }
                                 }
 
                             }
-
+                            while (!countryCodeIsCorrect);
                         }
-                        while (!countryCodeIsCorrect);
-                    }
                         Console.Write("Прошу ввести количество плитки в кв метрах: ");
                         string stringQuentity = Console.ReadLine();
                         tileQuentity = decimal.Parse(stringQuentity);
@@ -252,8 +323,9 @@ namespace TrainConsole22
                     }
 
 
-                
 
+
+                }
             }
 
         }
